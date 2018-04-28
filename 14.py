af62bcc886f2b6383f6e9cf6a86b3ae9c58c77d8
Kitt Holland <kittholland@gmail.com>
2018-04-28 07:49:22 -0700

    exercise 14

---
+++ 14.py
@@ -0,0 +1,22 @@
+def set_deduper(l):
+    return list(set(l))
+
+def loop_deduper(l):
+    results = []
+    for item in l:
+        if item not in results:
+            results.append(item)
+    return results
+
+l = [1,2,3,4,5,6,1,2,3,9,9,9]
+
+print(set_deduper(l))
+print(loop_deduper(l))
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def list_deduper(a, b):
+    return([number for number in a if number in b])
+
+print(list_deduper(a, b))
